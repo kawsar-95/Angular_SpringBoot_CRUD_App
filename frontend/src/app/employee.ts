48e166899b5fb01ddac032ac671a62f14d88ed81
kawsar-95 <virtualkawsar@gmail.com>
2021-09-06 11:08:12 +0600

    first commit

---
+++ frontend/src/app/employee.ts
@@ -0,0 +1,7 @@
+//Before defining the EmployeeListComponent, let’s define an Employee class for working with employees.
+export class Employee {
+  id!: number;
+  firstName!: string;
+  lastName!: string;
+  emailId!: string;
+}
