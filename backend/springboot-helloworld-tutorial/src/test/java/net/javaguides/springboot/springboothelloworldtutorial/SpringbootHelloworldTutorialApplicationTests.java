8109b06319dded7bac5ff6e9332e131e128bf1bb
kawsar-95 <nuruddinkawsar1995@gmail.com>
2021-09-07 18:42:15 +0600

    backend added

---
+++ backend/springboot-helloworld-tutorial/src/test/java/net/javaguides/springboot/springboothelloworldtutorial/SpringbootHelloworldTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.springboothelloworldtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHelloworldTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
